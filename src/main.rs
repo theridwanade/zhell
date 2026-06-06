d861bfb06325d3222e6ed010c991b8fd44ab2679
theridwanade <theridwanade@gmail.com>
2026-06-06 14:47:05 +0100

    Created the REPL loop, accepts user inputs and executed the exit builtin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+
+        if input.is_empty() {
+            continue;
+        }
+
+        let (cmd, args) = match input.split_once(" ") {
+            Some((c, a)) => (c, a.trim()),
+            None => (input, ""),
+        };
+
+        if cmd == "exit" {
+            break;
+        }
+    }
 }
