6c8bea343008510e257a16c8418e73587984d97b
theridwanade <theridwanade@gmail.com>
2026-06-12 22:57:04 +0100

    refactor: Simplify command execution and output handling by restructuring argument processing and command execution logic

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use std::{
-    fs::{File, OpenOptions},
-    io::{self, Write},
-    process::{Command, Stdio},
-};
 mod builtin;
 mod lexer;
 mod utils;
 
+use std::io::ErrorKind;
+
 use builtin::Builtin;
 use rustyline::{Editor, Result, error::ReadlineError, history::DefaultHistory};
 
-use crate::{lexer::tokenize, utils::{ZhellHelper, find_in_path}};
+use crate::{
+    lexer::tokenize,
+    utils::{ZhellHelper, execute_builtin_command, execute_external_command, process_raw_args},
+};
 
 fn main() -> Result<()> {
     let mut rl = Editor::<ZhellHelper, DefaultHistory>::new()?;
@@ -19,12 +19,29 @@ fn main() -> Result<()> {
     rl.set_helper(Some(helper));
     loop {
         let readline = rl.readline("$ ");
-
-        let mut input = String::new();
         match readline {
             Ok(line) => {
-                rl.add_history_entry(line.as_str())?;
-                input = line;
+                let _ = rl.add_history_entry(line.as_str());
+                let input = tokenize(line.trim());
+
+                if input.is_empty() {
+                    continue;
+                }
+
+                let (cmd, raw_args) = (input[0].as_str(), input[1..].to_vec());
+                let processed_args = process_raw_args(&raw_args);
+
+                if let Some(builtin) = Builtin::parse(cmd) {
+                    execute_builtin_command(builtin, processed_args);
+                } else {
+                    if let Err(e) = execute_external_command(cmd, processed_args) {
+                        if e.kind() == ErrorKind::NotFound {
+                           eprintln!("{}: command not found", cmd);
+                        } else {
+                            eprintln!("Error: {:?}", e);
+                        }
+                    }
+                }
             }
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");
@@ -39,186 +56,6 @@ fn main() -> Result<()> {
                 break;
             }
         }
-
-        let command_input = input.trim();
-
-        if command_input.is_empty() {
-            continue;
-        }
-
-        let cmd_token = tokenize(command_input);
-        if cmd_token.is_empty() {
-            continue;
-        }
-
-        let (cmd, raw_args) = (cmd_token[0].as_str(), cmd_token[1..].to_vec());
-
-        let mut actual_args = Vec::new();
-        let mut output_file = None;
-        let mut error_output_file = None;
-        let mut to_append_output = false;
-        let mut i = 0;
-
-        while i < raw_args.len() {
-            match raw_args[i].as_str() {
-                ">" | "1>" => {
-                    if i + 1 < raw_args.len() {
-                        output_file = Some(raw_args[i + 1].clone());
-                        i += 2;
-                    } else {
-                        eprintln!("Syntax error: expected file after '{}'", raw_args[i]);
-                        break;
-                    }
-                }
-                "2>" => {
-                    if i + 1 < raw_args.len() {
-                        error_output_file = Some(raw_args[i + 1].clone());
-                        i += 2;
-                    } else {
-                        eprintln!("Syntax error: expected file after '{}'", raw_args[i]);
-                        break;
-                    }
-                }
-                ">>" | "1>>" => {
-                    output_file = Some(raw_args[i + 1].clone());
-                    to_append_output = true;
-                    i += 2;
-                }
-                "2>>" => {
-                    error_output_file = Some(raw_args[i + 1].clone());
-                    to_append_output = true;
-                    i += 2;
-                }
-                _ => {
-                    actual_args.push(raw_args[i].clone());
-                    i += 1;
-                }
-            }
-        }
-
-        if let Some(builtin) = Builtin::parse(cmd) {
-            if let Some(ref file_path) = output_file {
-                if to_append_output {
-                    let _ = OpenOptions::new().create(true).append(true).open(file_path);
-                } else {
-                    let _ = File::create(file_path);
-                }
-            }
-            if let Some(ref file_path) = error_output_file {
-                if to_append_output {
-                    let _ = OpenOptions::new().create(true).append(true).open(file_path);
-                } else {
-                    let _ = File::create(file_path);
-                }
-            }
-            match builtin.execute(actual_args) {
-                Ok(output) => {
-                    if let Some(file_path) = output_file {
-                        let file_result = if to_append_output {
-                            OpenOptions::new()
-                                .create(true)
-                                .append(true)
-                                .open(&file_path)
-                        } else {
-                            File::create(&file_path)
-                        };
-
-                        match file_result {
-                            Ok(mut file) => match writeln!(file, "{}", output) {
-                                Ok(_) => {}
-                                Err(e) => eprintln!("Error writing to file: {}", e),
-                            },
-                            Err(e) => eprintln!("Error opening file {}: {}", file_path, e),
-                        }
-                    } else {
-                        if !output.is_empty() {
-                            println!("{}", output);
-                        }
-                    }
-                }
-                Err(e) => {
-                    if let Some(file_path) = error_output_file {
-                        let file_result = if to_append_output {
-                            OpenOptions::new()
-                                .create(true)
-                                .append(true)
-                                .open(&file_path)
-                        } else {
-                            File::create(&file_path)
-                        };
-
-                        match file_result {
-                            Ok(mut file) => match writeln!(file, "{}", e) {
-                                Ok(_) => {}
-                                Err(e) => eprintln!("Error writing to file: {}", e),
-                            },
-                            Err(e) => eprintln!("Error opening file {}: {}", file_path, e),
-                        }
-                    } else {
-                        eprintln!("{}", e)
-                    }
-                }
-            }
-            continue;
-        } else {
-            if let Some(_path) = find_in_path(cmd) {
-                let mut command = Command::new(cmd);
-                command.args(actual_args);
-
-                if let Some(file_path) = output_file {
-                    let file_result = if to_append_output {
-                        OpenOptions::new()
-                            .create(true)
-                            .append(true)
-                            .open(&file_path)
-                    } else {
-                        File::create(&file_path)
-                    };
-
-                    match file_result {
-                        Ok(file) => {
-                            command.stdout(Stdio::from(file));
-                        }
-                        Err(e) => {
-                            eprintln!("Error opening file {}: {}", file_path, e);
-                            continue;
-                        }
-                    }
-                }
-
-                if let Some(file_path) = error_output_file {
-                    let file_result = if to_append_output {
-                        OpenOptions::new()
-                            .create(true)
-                            .append(true)
-                            .open(&file_path)
-                    } else {
-                        File::create(&file_path)
-                    };
-                    match file_result {
-                        Ok(file) => {
-                            command.stderr(Stdio::from(file));
-                        }
-                        Err(e) => {
-                            eprintln!("Error opening file {}: {}", file_path, e);
-                            continue;
-                        }
-                    }
-                }
-
-                match command.spawn() {
-                    Ok(mut child_process) => {
-                        let _ = child_process.wait();
-                    }
-                    Err(e) => {
-                        eprintln!("Error executing command: {}", e);
-                    }
-                }
-                continue;
-            }
-        }
-
-        println!("{}: command not found", cmd);
     }
     let _ = rl.save_history("history.txt");
     Ok(())
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,12 @@
-use std::{env, fs, os::unix::fs::PermissionsExt, path::PathBuf};
-
 use rustyline::{Context, completion::Completer};
+use std::io::{Error, ErrorKind, Result, Write};
+use std::{
+    env,
+    fs::{self, File, OpenOptions},
+    os::unix::fs::PermissionsExt,
+    path::PathBuf,
+    process::{Command, Stdio},
+};
 
 use crate::builtin::Builtin;
 
@@ -48,4 +54,165 @@ impl Completer for ZhellHelper {
 
         Ok((word_start, candidates))
     }
+}
+
+pub struct RawArgs {
+    pub actual_args: Vec<String>,
+    pub output_file: Option<String>,
+    pub error_output_file: Option<String>,
+    pub to_append_output: bool,
+}
+
+pub fn process_raw_args(raw_args: &Vec<String>) -> RawArgs {
+    let mut actual_args = Vec::new();
+    let mut output_file = None;
+    let mut error_output_file = None;
+    let mut to_append_output = false;
+    let mut i = 0;
+
+    while i < raw_args.len() {
+        match raw_args[i].as_str() {
+            ">" | "1>" => {
+                if i + 1 < raw_args.len() {
+                    output_file = Some(raw_args[i + 1].clone());
+                    i += 2;
+                } else {
+                    eprintln!("Syntax error: expected file after '{}'", raw_args[i]);
+                    break;
+                }
+            }
+            "2>" => {
+                if i + 1 < raw_args.len() {
+                    error_output_file = Some(raw_args[i + 1].clone());
+                    i += 2;
+                } else {
+                    eprintln!("Syntax error: expected file after '{}'", raw_args[i]);
+                    break;
+                }
+            }
+            ">>" | "1>>" => {
+                output_file = Some(raw_args[i + 1].clone());
+                to_append_output = true;
+                i += 2;
+            }
+            "2>>" => {
+                error_output_file = Some(raw_args[i + 1].clone());
+                to_append_output = true;
+                i += 2;
+            }
+            _ => {
+                actual_args.push(raw_args[i].clone());
+                i += 1;
+            }
+        }
+    }
+
+    RawArgs {
+        actual_args,
+        output_file,
+        error_output_file,
+        to_append_output,
+    }
+}
+
+pub fn execute_builtin_command(builtin: Builtin, args: RawArgs) {
+    if let Some(ref file_path) = args.output_file {
+        if args.to_append_output {
+            let _ = OpenOptions::new().create(true).append(true).open(file_path);
+        } else {
+            let _ = File::create(file_path);
+        }
+    }
+    if let Some(ref file_path) = args.error_output_file {
+        if args.to_append_output {
+            let _ = OpenOptions::new().create(true).append(true).open(file_path);
+        } else {
+            let _ = File::create(file_path);
+        }
+    }
+    match builtin.execute(args.actual_args) {
+        Ok(output) => {
+            if let Some(file_path) = args.output_file {
+                let file_result = if args.to_append_output {
+                    OpenOptions::new()
+                        .create(true)
+                        .append(true)
+                        .open(&file_path)
+                } else {
+                    File::create(&file_path)
+                };
+
+                match file_result {
+                    Ok(mut file) => match writeln!(file, "{}", output) {
+                        Ok(_) => {}
+                        Err(e) => eprintln!("Error writing to file: {}", e),
+                    },
+                    Err(e) => eprintln!("Error opening file {}: {}", file_path, e),
+                }
+            } else {
+                if !output.is_empty() {
+                    println!("{}", output);
+                }
+            }
+        }
+        Err(e) => {
+            if let Some(file_path) = args.error_output_file {
+                let file_result = if args.to_append_output {
+                    OpenOptions::new()
+                        .create(true)
+                        .append(true)
+                        .open(&file_path)
+                } else {
+                    File::create(&file_path)
+                };
+
+                match file_result {
+                    Ok(mut file) => match writeln!(file, "{}", e) {
+                        Ok(_) => {}
+                        Err(e) => eprintln!("Error writing to file: {}", e),
+                    },
+                    Err(e) => eprintln!("Error opening file {}: {}", file_path, e),
+                }
+            } else {
+                eprintln!("{}", e)
+            }
+        }
+    }
+}
+
+pub fn execute_external_command(cmd: &str, args: RawArgs) -> Result<()> {
+    if find_in_path(cmd).is_none() {
+        return Err(Error::new(ErrorKind::NotFound, "command not found"));
+    }
+    let mut command = Command::new(cmd);
+    command.args(args.actual_args);
+
+    if let Some(file_path) = args.output_file {
+        let file = if args.to_append_output {
+            OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(&file_path)?
+        } else {
+            File::create(&file_path)?
+        };
+        command.stdout(Stdio::from(file));
+    }
+
+    if let Some(file_path) = args.error_output_file {
+        let file = if args.to_append_output {
+            OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(&file_path)?
+        } else {
+            File::create(&file_path)?
+        };
+        command.stderr(Stdio::from(file));
+    }
+
+    let mut child_process = command.spawn()?;
+    child_process.wait()?;
+
+    Ok(())
 }
